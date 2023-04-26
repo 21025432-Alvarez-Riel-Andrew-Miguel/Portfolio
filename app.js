04ae54b29ea21f270652075e773cd9c36c5b0316
21025432-Alvarez-Riel-Andrew-Miguel <21025432@myrpedu.sg>
2023-04-27 00:06:21 +0800

    updated portfolio page 2

--- app.js
+++ app.js
@@ -29,3 +29,8 @@ const observer = new IntersectionObserver(entries => {
 
 me.forEach((el) => observer.observe(el))
 aboutMe.forEach((el) => observer.observe(el))
+
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+  }
